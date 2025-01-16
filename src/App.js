7c07027831750b61205ce133ccd4cf6eac98cd37
moiz1994 <moizwaseem1994@gmail.com>
2025-01-16 10:35:54 +0500

    Admin Dashboard With Topbar and All the routes

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import TopBar from "./scenes/global/TopBar";
+import SideBar from "./scenes/global/SideBar";
+import DashBoard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import Faq from "./scenes/faq";
+import Geography from "./scenes/geography";
+import Calendar from "./scenes/calendar";
+
+const App = () => {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <TopBar />
+            <Routes>
+              <Route path="/" element={<DashBoard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/faq" element={<Faq />} />
+              <Route path="/geography" element={<Geography />} />
+              <Route path="/calendar" element={<Calendar />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
+
+export default App;
