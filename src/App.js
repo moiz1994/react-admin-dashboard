fd0516cd7a7f8951d28c02dda6350e6bbd0bc9a4
moiz1994 <moizwaseem1994@gmail.com>
2025-01-16 16:59:59 +0500

    Completed Sidebar and TopBar Design and Started WOrking on Team page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { Route, Routes } from "react-router-dom";
 import TopBar from "./scenes/global/TopBar";
 import SideBar from "./scenes/global/SideBar";
 import DashBoard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import Faq from "./scenes/faq";
-import Geography from "./scenes/geography";
-import Calendar from "./scenes/calendar";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import Faq from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 const App = () => {
   const [theme, colorMode] = useMode();
@@ -22,20 +22,21 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <SideBar />
           <main className="content">
             <TopBar />
             <Routes>
               <Route path="/" element={<DashBoard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/faq" element={<Faq />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/calendar" element={<Calendar />} />
+              {/* <Route path="/team" element={<Team />} /> */}
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/faq" element={<Faq />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
             </Routes>
           </main>
         </div>
--- src/scenes/dashboard/index.js
+++ src/scenes/dashboard/index.js
@@ -1,5 +1,13 @@
-const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subTitle="Welcome to you Dashboard" />
+      </Box>
+    </Box>
+  );
+};
 export default Dashboard;
